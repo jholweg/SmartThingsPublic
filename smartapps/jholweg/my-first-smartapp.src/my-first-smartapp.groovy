cd651edc37af03b7db81866a30c943efea081a47
Jaap Holweg <jaap@holweg.nu>
2019-02-06 14:59:02 +0100

    Second feature test

--- smartapps/jholweg/my-first-smartapp.src/my-first-smartapp.groovy
+++ smartapps/jholweg/my-first-smartapp.src/my-first-smartapp.groovy
@@ -36,6 +36,7 @@ preferences {
     }
     section("New feature test - explode in") {
         input "explodein", "number", required: true, title: "Minutes?"
+        input "delay", "number", required: true, title: "Delay Minutes?"
     }
 }
 
